5010e802d49a9e87fd7fd703e2d6be3425956d43
Yurii Shynbuiev <yurii.shynbuiev@iohk.io>
2026-05-22 04:44:16 +0800

    refactor(api): isolate wallet state helpers
    Signed-off-by: Yurii Shynbuiev <yurii.shynbuiev@iohk.io>

--- packages/api/src/wallet.ts
+++ packages/api/src/wallet.ts
@@ -1,13 +1,9 @@
 import "./polyfills.js";
 
 import * as ledger from "@midnight-ntwrk/ledger-v8";
-import { unshieldedToken } from "@midnight-ntwrk/ledger-v8";
 import { toHex } from "@midnight-ntwrk/midnight-js-utils";
 import { DustWallet } from "@midnight-ntwrk/wallet-sdk-dust-wallet";
-import {
-  type FacadeState,
-  WalletFacade,
-} from "@midnight-ntwrk/wallet-sdk-facade";
+import { WalletFacade } from "@midnight-ntwrk/wallet-sdk-facade";
 import { Roles } from "@midnight-ntwrk/wallet-sdk-hd";
 import { ShieldedWallet } from "@midnight-ntwrk/wallet-sdk-shielded";
 import {
@@ -15,15 +11,12 @@ import {
   PublicKey,
   UnshieldedWallet,
 } from "@midnight-ntwrk/wallet-sdk-unshielded-wallet";
-import * as Rx from "rxjs";
 
 import { getLogger } from "./api-logger.js";
 import { type Config } from "./config.js";
 import { randomBytes } from "./lightweight.js";
 import {
   type MidnightDIDWalletContext,
-  type MidnightWalletBalances,
-  type MidnightWalletFacadeState,
   type MidnightWalletStateSnapshot,
 } from "./types.js";
 import {
@@ -35,8 +28,15 @@ import {
   buildShieldedConfig,
   buildUnshieldedConfig,
 } from "./wallet-sdk-config.js";
+import { waitForWalletFunds, waitForWalletSync } from "./wallet-state.js";
 
 export { registerForDustGeneration } from "./wallet-dust.js";
+export {
+  getWalletBalances,
+  serializeWalletState,
+  waitForWalletFunds,
+  waitForWalletSync,
+} from "./wallet-state.js";
 
 const createWalletContext = async (
   config: Config,
@@ -107,54 +107,6 @@ const createWalletContext = async (
     dustSecretKey,
     unshieldedKeystore,
   };
-};
-
-export const serializeWalletState = async (
-  ctx: MidnightDIDWalletContext,
-): Promise<MidnightWalletStateSnapshot> => ({
-  shieldedState: await ctx.shieldedWallet.serializeState(),
-  unshieldedState: await ctx.unshieldedWallet.serializeState(),
-  dustState: await ctx.dustWallet.serializeState(),
-  unshieldedHistory: ctx.unshieldedHistoryStorage.serialize(),
-});
-
-export const waitForWalletSync = async (
-  ctx: MidnightDIDWalletContext,
-): Promise<FacadeState> => {
-  getLogger().info("Waiting for wallet sync...");
-  return await ctx.wallet.waitForSyncedState();
-};
-
-export const getWalletBalances = (
-  state: MidnightWalletFacadeState,
-): MidnightWalletBalances => {
-  if (!state.isSynced) {
-    return {
-      night: null,
-      dust: null,
-    };
-  }
-
-  return {
-    night: state.unshielded.balances[unshieldedToken().raw] ?? 0n,
-    dust: state.dust.balance(new Date()),
-  };
-};
-
-export const waitForWalletFunds = async (
-  ctx: MidnightDIDWalletContext,
-): Promise<bigint> => {
-  getLogger().info("Waiting for funds...");
-  const balance = await Rx.firstValueFrom(
-    ctx.wallet.state().pipe(
-      Rx.throttleTime(10_000),
-      Rx.map(getWalletBalances),
-      Rx.map((balances) => balances.night ?? 0n),
-      Rx.filter((currentBalance) => currentBalance > 0n),
-    ),
-  );
-  getLogger().info(`Wallet balance: ${balance}`);
-  return balance;
 };
 
 export const buildWallet = async (
