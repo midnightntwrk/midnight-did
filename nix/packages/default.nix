6fe400b403406a3f6b5ba078099ddc7edaee8bc1
Pat Losoponkul <patextreme@hotmail.com>
2026-05-27 13:08:56 +0700

    feat: add Nix package for npm artifact tarballs (#159)
    * Add Nix packages for compact toolchain, circuit params, and npm artifacts
    
    * Simplify npm artifacts build and add BLS circuit params
    
    * Add source_up to .envrc for parent environment inheritance
    
    * Switch Nix build from npm to pnpm and remove lockfile patching

--- nix/packages/default.nix
+++ nix/packages/default.nix
@@ -1,9 +1,20 @@
+{ self, ... }:
+
 {
   perSystem =
     { pkgs, ... }:
+    let
+      compact-midnight = pkgs.callPackage ./compact-midnight.nix { };
+      compact-toolchain = pkgs.callPackage ./compact-toolchain.nix { };
+      midnight-circuit-params = pkgs.callPackage ./midnight-circuit-params.nix { };
+    in
     {
       packages = {
-        compact-midnight = pkgs.callPackage ./compact-midnight.nix { };
+        inherit compact-midnight compact-toolchain midnight-circuit-params;
+        npm-artifacts = pkgs.callPackage ./npm-artifacts.nix {
+          inherit compact-midnight compact-toolchain midnight-circuit-params;
+          src = self;
+        };
       };
     };
 }
